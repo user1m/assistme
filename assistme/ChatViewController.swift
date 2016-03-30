dbf8257858471eab390adfdc534fbfefbe3cdc8e
Claudius Mbemba <mbembac@gmail.com>
2016-03-30 02:07:16 -0700

    added demo messaging

--- assistme/ChatViewController.swift
+++ assistme/ChatViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ChatViewController: UIViewController {
+
+class ChatViewController: JSQMessagesViewController {
   
-  @IBOutlet weak var chatView: UITextView!
-  @IBOutlet weak var chatInput: UITextField!
   
   let api = API.getSingleton()
   var currentDialog:[String:String] = [:]
   var client_id:Int = 0, conversation_id:Int = 0
+  
+  let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImageWithColor(UIColor(red: 10/255, green: 180/255, blue: 230/255, alpha: 1.0))
+  let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor.lightGrayColor())
+  var messages = [JSQMessage]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -23,9 +27,10 @@ class ChatViewController: UIViewController {
     // Do any additional setup after loading the view.
     
     title =  "Pizza Chat Bot"
-    self.chatView.text = ""
     
-    self.getConvoData()
+    //    self.getConvoData()
+    self.setup()
+    self.addDemoMessages()
   }
   
   override func didReceiveMemoryWarning() {
@@ -42,7 +47,7 @@ class ChatViewController: UIViewController {
   func updateConverationView(responses:NSArray){
     for resp in responses{
       if let resp = resp as? String{
-        self.chatInput += "\(resp))\n"
+        //        self.chatInput += "\(resp))\n"
       }
     }
   }
@@ -70,13 +75,16 @@ class ChatViewController: UIViewController {
   
   @IBAction func sendChat() {
     var params = ""
-    if client_id != 0 && conversation_id != 0 {
-      params = "input=\(self.chatInput.text)&client_id=\(self.client_id)&conversation_id=\(self.conversation_id)"
-    }
+    //    if client_id != 0 && conversation_id != 0 {
+    //      params = "input=\(self.chatInput.text)&client_id=\(self.client_id)&conversation_id=\(self.conversation_id)"
+    //    }
     
     self.getConvoData(params)
   }
   
+  func reloadMessagesView() {
+    self.collectionView?.reloadData()
+  }
   
   /*
    // MARK: - Navigation
@@ -89,3 +97,59 @@ class ChatViewController: UIViewController {
    */
   
 }
+
+extension ChatViewController {
+  func addDemoMessages() {
+    for i in 1...10 {
+      let sender = (i%2 == 0) ? "Server" : self.senderId
+      let messageContent = "Message nr. \(i)"
+      let message = JSQMessage(senderId: sender, displayName: sender, text: messageContent)
+      self.messages += [message]
+    }
+    self.reloadMessagesView()
+  }
+  
+  func setup() {
+    self.senderId = UIDevice.currentDevice().identifierForVendor?.UUIDString
+    self.senderDisplayName = UIDevice.currentDevice().identifierForVendor?.UUIDString
+  }
+  
+  override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return self.messages.count
+  }
+  
+  override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+    let data = self.messages[indexPath.row]
+    return data
+  }
+  
+  override func collectionView(collectionView: JSQMessagesCollectionView!, didDeleteMessageAtIndexPath indexPath: NSIndexPath!) {
+    self.messages.removeAtIndex(indexPath.row)
+  }
+  
+  override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+    let data = messages[indexPath.row]
+    switch(data.senderId) {
+    case self.senderId:
+      return self.outgoingBubble
+    default:
+      return self.incomingBubble
+    }
+  }
+  
+  override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+    return nil
+  }
+  
+  override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+    let message = JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)
+    self.messages += [message]
+    self.finishSendingMessage()
+  }
+  
+  override func didPressAccessoryButton(sender: UIButton!) {
+    
+  }
+}
+
+
