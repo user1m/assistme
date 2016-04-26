9214c9cbdb5990720671d90df8b053982f4abfd4
Claudius Mbemba <mbembac@gmail.com>
2016-04-25 19:33:45 -0700

    removed commecnts

--- assistme/API.swift
+++ assistme/API.swift
@@ -32,14 +32,6 @@ public class API {
   public var dialogs:[[String:String]]!
   public var conversations:[[String:AnyObject]]!
   
-  //  static func getSharedInstance() -> API {
-  //    if ( sharedInstance != nil) {
-  //      return sharedInstance!;
-  //    }
-  //    sharedInstance = API()
-  //    return sharedInstance!
-  //  }
-  
   static func getDialogs(completionHandler: ((NSArray!, NSError!) -> Void)!) -> Void
   {
     
@@ -50,36 +42,6 @@ public class API {
           return completionHandler(JSON as! NSArray , nil)
         }
     }
-    
-    //    let url = NSURL(string: "\(baseApi)\(dialog)")!
-    //
-    //    session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-    //
-    //      guard let realResponse = response as? NSHTTPURLResponse where
-    //        realResponse.statusCode == 200 else {
-    //          print(response)
-    //          print("Not a 200 response")
-    //
-    //          dispatch_async(dispatch_get_main_queue(), {
-    //            HUD.flash(.Error, delay: 0.5)
-    //          })
-    //          return
-    //      }
-    //
-    //      // Read the JSON
-    //      do {
-    //        if let result = NSString(data:data!, encoding: NSUTF8StringEncoding) {
-    //          // print(result)
-    //          // Parse the JSON to get the data
-    //          let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [NSDictionary]
-    //          //          self.performSelectorOnMainThread(#selector(API.gotData(_:)), withObject: jsonDictionary, waitUntilDone: true)
-    //          return completionHandler(json, nil)
-    //        }
-    //      } catch {
-    //        error
-    //        //        print("Error: Failed to getdialogs ")
-    //      }
-    //    }).resume()
   }
   
   // Setup the session to make REST GET call. (HTTPS vs HTTP)
@@ -92,40 +54,6 @@ public class API {
           return completionHandler(JSON as! [String : AnyObject], nil)
         }
     }
-    
-    //    var url:NSURL! = NSURL()
-    //    if (params != nil && !params!.isEmpty)
-    //    {
-    //      url = NSURL(string: "\(baseApi)\(convo)?\(params!.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)")
-    //    } else {
-    //      url = NSURL(string: "\(baseApi)\(convo)")
-    //    }
-    //    session.dataTaskWithURL(url!, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-    //
-    //      guard let realResponse = response as? NSHTTPURLResponse where
-    //        realResponse.statusCode == 200 else {
-    //          print(response)
-    //          print("Not a 200 response")
-    //
-    //          dispatch_async(dispatch_get_main_queue(), {
-    //            HUD.flash(.Error, delay: 0.5)
-    //          })
-    //          return
-    //      }
-    //
-    //      // Read the JSON
-    //      do {
-    //        if let result = NSString(data:data!, encoding: NSUTF8StringEncoding) {
-    //          // print(result)
-    //          // Parse the JSON to get the data
-    //          let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
-    //          return completionHandler(json, nil)
-    //        }
-    //      } catch {
-    //        error
-    //        //        print("Error: Failed to get conversation ")
-    //      }
-    //    }).resume()
   }
   
   //  static func classify( completionHandler: (([String:AnyObject]!, NSError!) -> Void)!) -> Void
@@ -138,80 +66,5 @@ public class API {
           return handler(reply: JSON["reply"] as! String)
         }
     }
-    //    let url = NSURL(string: "\(API.baseApi)classify")
-    //    session.dataTaskWithURL(url!, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-    //
-    //      guard let realResponse = response as? NSHTTPURLResponse where
-    //        realResponse.statusCode == 200 else {
-    //          print(response)
-    //          print("Not a 200 response")
-    //
-    //          dispatch_async(dispatch_get_main_queue(), {
-    //            HUD.flash(.Error, delay: 0.5)
-    //          })
-    //          return
-    //      }
-    //
-    //      // Read the JSON
-    //      do {
-    //        if let result = NSString(data:data!, encoding: NSUTF8StringEncoding) {
-    //          print(result)
-    //
-    //          // Parse the JSON to get the data
-    //          let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
-    //          return completionHandler(json, nil)
-    //        }
-    //      } catch {
-    //        error
-    //        //        print("Error: Failed to get conversation ")
-    //      }
-    //    }).resume()
   }
-  
-  
-  // Setup the session to make REST POST call
-  //  func postConversation(endpoint:String, input:String) {
-  //    let url = NSURL(string: endpoint)!
-  //    //    var postParams:[String:String] = [:]
-  //    var bodyStr:String = ""
-  //    // Create the request
-  //    let request = NSMutableURLRequest(URL: url)
-  //
-  //    if let client_id:String = self.client_id! , conversation_id:String = self.conversation_id!{
-  //      //       postParams = ["input": input, "client_id": client_id, "conversation_id":conversation_id]
-  //      bodyStr = "input=\(input),client_id=\(client_id),converstation_id=\(conversation_id)"
-  //    }
-  //
-  //    request.HTTPMethod = "POST"
-  //    //    request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-  //    request.HTTPBody = bodyStr.dataUsingEncoding(NSUTF8StringEncoding)
-  //    request.setValue("application/json", forHTTPHeaderField: "Accept")
-  //    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-  //
-  //    //    do {
-  //    ////      request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(postParams, options: NSJSONWritingOptions())
-  //    ////      print(postParams)
-  //    //    } catch {
-  //    //      print("bad things happened")
-  //    //    }
-  //
-  //    // Make the POST call and handle it in a completion handler
-  //    session.dataTaskWithRequest(request, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-  //      // Make sure we get an OK response
-  //      guard let realResponse = response as? NSHTTPURLResponse where
-  //        realResponse.statusCode == 200 else {
-  //          print("Not a 200 response")
-  //          return
-  //      }
-  //
-  //      // Read the JSON
-  //      if let result = NSString(data:data!, encoding: NSUTF8StringEncoding) as? String {
-  //        // Print what we got from the call
-  //        print("POST: " + result)
-  //        //        self.performSelectorOnMainThread("updatePostLabel:", withObject: postString, waitUntilDone: false)
-  //      }
-  //
-  //    }).resume()
-  //
-  //  }
 }
